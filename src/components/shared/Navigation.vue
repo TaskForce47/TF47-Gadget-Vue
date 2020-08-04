35c60eaa30882a3ba122d57aea6327e82b2f3545
RednecksRevenge <deoflasche63@gmail.com>
2020-08-04 19:19:29 +0200

    extended role based visibility

--- src/components/shared/Navigation.vue
+++ src/components/shared/Navigation.vue
@@ -2,40 +2,58 @@
 	<div>
 		<template v-if="!burger">
 			<template v-for="(navlink, index) in navigationLink.links">
-				<template v-if="navlink && navlink.tooltip">
-					<v-tooltip bottom :key="index">
-						<template v-slot:activator="{ on }">
-							<v-btn min-width="0" text v-on="on" :to="navlink.to" class="ml-4">
-								<v-icon>{{ navlink.icon }}</v-icon>
-							</v-btn>
-						</template>
-						<span>{{ navlink.tooltip }}</span>
-					</v-tooltip>
-				</template>
-				<template v-if="navlink && navlink.sublinks && navlink.sublinks.length > 0">
-					<v-menu
-						open-on-hover
-						offset-y
-						:key="index"
-						v-if="$tstore.state.auth.roles.some(role => navlink.roles.indexOf(role) >= 0)"
-					>
-						<template v-slot:activator="{ on }">
-							<v-btn color="info" text dark v-on="on">
-								{{ navlink.name }}
-							</v-btn>
-						</template>
-						<v-list dense>
-							<v-list-item-group>
-								<template v-for="(sublink, sublinkIndex) in navlink.sublinks">
-									<v-list-item :key="sublinkIndex" :to="sublink.to">
-										<v-list-item-icon>
-											<v-icon>{{ sublink.icon }}</v-icon>
-										</v-list-item-icon>
-										<v-list-item-content>
-											<v-list-item-title>{{ sublink.name }}</v-list-item-title>
-										</v-list-item-content>
-									</v-list-item>
-								</template>
+        <template
+            v-if="
+						navlink.roles.length === 0 ||
+							$tstore.state.auth.roles.some(role => navlink.roles.indexOf(role) >= 0)
+					"
+        >
+          <template v-if="navlink && navlink.tooltip">
+            <v-tooltip :key="index" bottom>
+              <template v-slot:activator="{ on }">
+                <v-btn :to="navlink.to" class="ml-4" min-width="0" text v-on="on">
+                  <v-icon>{{ navlink.icon }}</v-icon>
+                </v-btn>
+              </template>
+              <span>{{ navlink.tooltip }}</span>
+            </v-tooltip>
+          </template>
+        </template>
+
+        <template v-if="navlink && navlink.sublinks && navlink.sublinks.length > 0">
+          <v-menu
+              open-on-hover
+              offset-y
+              :key="index"
+              v-if="
+							navlink.roles.length === 0 ||
+								$tstore.state.auth.roles.some(role => navlink.roles.indexOf(role) >= 0)
+						"
+          >
+            <template v-slot:activator="{ on }">
+              <v-btn color="info" text dark v-on="on">
+                {{ navlink.name }}
+              </v-btn>
+            </template>
+            <v-list dense>
+              <v-list-item-group>
+                <template v-for="(sublink, sublinkIndex) in navlink.sublinks">
+                  <v-list-item
+                      :key="sublinkIndex"
+                      :to="sublink.to"
+                      v-if="
+											sublink.roles.length === 0 ||
+												$tstore.state.auth.roles.some(role => sublink.roles.indexOf(role) >= 0)
+										"
+                  >
+                    <v-list-item-icon>
+                      <v-icon>{{ sublink.icon }}</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                      <v-list-item-title>{{ sublink.name }}</v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                </template>
 							</v-list-item-group>
 						</v-list>
 					</v-menu>
@@ -56,37 +74,54 @@
 				width="85%"
 			>
 				<v-list nav>
-					<template v-for="(navlink, index) in navigationLink.links">
-						<template v-if="navlink && navlink.tooltip">
-							<v-list-item :key="index" :to="navlink.to">
-								<v-list-item-icon>
-									<v-icon>{{ navlink.icon }}</v-icon>
-								</v-list-item-icon>
+          <template v-for="(navlink, index) in navigationLink.links">
+            <template
+                v-if="
+								navlink.roles.length === 0 ||
+									$tstore.state.auth.roles.some(role => navlink.roles.indexOf(role) >= 0)
+							"
+            >
+              <template v-if="navlink && navlink.tooltip">
+                <v-list-item :key="index" :to="navlink.to">
+                  <v-list-item-icon>
+                    <v-icon>{{ navlink.icon }}</v-icon>
+                  </v-list-item-icon>
 
-								<v-list-item-title>{{ navlink.tooltip }}</v-list-item-title>
-							</v-list-item>
-						</template>
-						<template v-if="navlink && navlink.sublinks && navlink.sublinks.length > 0">
-							<v-list
-								:key="index"
-								v-if="$tstore.state.auth.roles.some(role => navlink.roles.indexOf(role) >= 0)"
-							>
-								<v-list-group no-action value="true" class="text-wrap" color="info">
-									<template v-slot:activator>
-										<v-list-item-title>{{ navlink.name }}</v-list-item-title>
-									</template>
-									<v-list-item
-										v-for="(sublink, sublinkIndex) in navlink.sublinks"
-										:key="sublinkIndex"
-										:to="sublink.to"
-										link
-									>
-										<v-list-item-icon>
-											<v-icon>{{ sublink.icon }}</v-icon>
-										</v-list-item-icon>
-										<v-list-item-title>{{ sublink.name }}</v-list-item-title>
-									</v-list-item>
-								</v-list-group>
+                  <v-list-item-title>{{ navlink.tooltip }}</v-list-item-title>
+                </v-list-item>
+              </template>
+            </template>
+            <template v-if="navlink && navlink.sublinks && navlink.sublinks.length > 0">
+              <v-list
+                  :key="index"
+                  v-if="
+									navlink.roles.length === 0 ||
+										$tstore.state.auth.roles.some(role => navlink.roles.indexOf(role) >= 0)
+								"
+              >
+                <v-list-group no-action value="true" class="text-wrap" color="info">
+                  <template v-slot:activator>
+                    <v-list-item-title>{{ navlink.name }}</v-list-item-title>
+                  </template>
+                  <template v-for="(sublink, sublinkIndex) in navlink.sublinks">
+                    <v-list-item
+                        :key="sublinkIndex"
+                        :to="sublink.to"
+                        link
+                        v-if="
+												sublink.roles.length === 0 ||
+													$tstore.state.auth.roles.some(
+														role => sublink.roles.indexOf(role) >= 0
+													)
+											"
+                    >
+                      <v-list-item-icon>
+                        <v-icon>{{ sublink.icon }}</v-icon>
+                      </v-list-item-icon>
+                      <v-list-item-title>{{ sublink.name }}</v-list-item-title>
+                    </v-list-item>
+                  </template>
+                </v-list-group>
 							</v-list>
 						</template>
 					</template>
